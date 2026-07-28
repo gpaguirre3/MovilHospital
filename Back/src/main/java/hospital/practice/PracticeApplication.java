7886af167f2658f6dceca2d3c0a60fd1b7237772
Gabriel Patricio Aguirre Guanoluisa <65300973+gpaguirre3@users.noreply.github.com>
2026-07-27 21:45:58 -0500

    new back java new

---
+++ Back/src/main/java/hospital/practice/PracticeApplication.java
@@ -0,0 +1,13 @@
+package hospital.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeApplication.class, args);
+	}
+
+}
